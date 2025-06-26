37d8972668d9143c4450406b1c4200b8f6110845
Peter Vermeulen <contact@valkyriestudios.be>
2025-06-26 10:08:04 +0200

    [chore] Minor tweak to new synth background

--- src/components/atoms/SynthBackground.tsx
+++ src/components/atoms/SynthBackground.tsx
@@ -14,8 +14,8 @@ export function SynthBackground() {
       inset: 0,
       backgroundImage: css.$t.synth_lines,
       backgroundSize: '50px 50px',
-      transform: 'translateY(600px) translateX(0) perspective(1400px) rotateX(70deg)',
-      maskImage: 'radial-gradient(circle at center, rgba(255,255,255,1) 30%, rgba(255,255,255,0) 80%)',
+      transform: 'translateY(500px) translateX(0) perspective(500px) rotateX(50deg)',
+      maskImage: 'radial-gradient(circle at center, rgba(255,255,255,1) 5%, rgba(255,255,255,0) 50%)',
     },
   });
   return <div className={cls}></div>;
