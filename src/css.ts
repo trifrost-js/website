b735e4ed5b71abf7a5711a8d9cfb0f79c2d13ce7
Peter Vermeulen <contact@valkyriestudios.be>
2025-07-03 12:12:59 +0200

    [chore] Adjust definitions to 0.43.x

--- src/css.ts
+++ src/css.ts
@@ -191,116 +191,116 @@ const _css = createCss({
   reset: true,
   definitions: mod => ({
     /* Flex */
-    f: {display: 'flex'},
-    fi: {display: 'inline-flex'},
-    fh: {flexDirection: 'row'},
-    fv: {flexDirection: 'column'},
-    fa_l: {alignItems: 'flex-start'},
-    fa_c: {alignItems: 'center'},
-    fa_r: {alignItems: 'flex-end'},
-    fj_l: {justifyContent: 'flex-start'},
-    fj_c: {justifyContent: 'center'},
-    fj_r: {justifyContent: 'flex-end'},
-    fj_sa: {justifyContent: 'space-around'},
-    fj_sb: {justifyContent: 'space-between'},
-    fw: {flexWrap: 'wrap'},
-    fg: {flexGrow: 1},
-    fg0: {flexGrow: 0},
-    fs: {flexShrink: 1},
-    fs0: {flexShrink: 0},
+    f: () => ({display: 'flex'}),
+    fi: () => ({display: 'inline-flex'}),
+    fh: () => ({flexDirection: 'row'}),
+    fv: () => ({flexDirection: 'column'}),
+    fa_l: () => ({alignItems: 'flex-start'}),
+    fa_c: () => ({alignItems: 'center'}),
+    fa_r: () => ({alignItems: 'flex-end'}),
+    fj_l: () => ({justifyContent: 'flex-start'}),
+    fj_c: () => ({justifyContent: 'center'}),
+    fj_r: () => ({justifyContent: 'flex-end'}),
+    fj_sa: () => ({justifyContent: 'space-around'}),
+    fj_sb: () => ({justifyContent: 'space-between'}),
+    fw: () => ({flexWrap: 'wrap'}),
+    fg: () => ({flexGrow: 1}),
+    fg0: () => ({flexGrow: 0}),
+    fs: () => ({flexShrink: 1}),
+    fs0: () => ({flexShrink: 0}),
     /* Radii */
-    br_s: {borderRadius: mod.$v.rad_s},
-    br_m: {borderRadius: mod.$v.rad_m},
-    br_l: {borderRadius: mod.$v.rad_l},
+    br_s: () => ({borderRadius: mod.$v.rad_s}),
+    br_m: () => ({borderRadius: mod.$v.rad_m}),
+    br_l: () => ({borderRadius: mod.$v.rad_l}),
     /* Spacing */
-    sp_xs: {padding: mod.$v.space_xs},
-    sp_s: {padding: mod.$v.space_s},
-    sp_m: {padding: mod.$v.space_m},
-    sp_l: {padding: mod.$v.space_l},
-    sp_xl: {padding: mod.$v.space_xl},
+    sp_xs: () => ({padding: mod.$v.space_xs}),
+    sp_s: () => ({padding: mod.$v.space_s}),
+    sp_m: () => ({padding: mod.$v.space_m}),
+    sp_l: () => ({padding: mod.$v.space_l}),
+    sp_xl: () => ({padding: mod.$v.space_xl}),
     /* Spacing Top */
-    sp_t_xs: {paddingTop: mod.$v.space_xs},
-    sp_t_s: {paddingTop: mod.$v.space_s},
-    sp_t_m: {paddingTop: mod.$v.space_m},
-    sp_t_l: {paddingTop: mod.$v.space_l},
-    sp_t_xl: {paddingTop: mod.$v.space_xl},
+    sp_t_xs: () => ({paddingTop: mod.$v.space_xs}),
+    sp_t_s: () => ({paddingTop: mod.$v.space_s}),
+    sp_t_m: () => ({paddingTop: mod.$v.space_m}),
+    sp_t_l: () => ({paddingTop: mod.$v.space_l}),
+    sp_t_xl: () => ({paddingTop: mod.$v.space_xl}),
     /* Spacing Right */
-    sp_r_xs: {paddingRight: mod.$v.space_xs},
-    sp_r_s: {paddingRight: mod.$v.space_s},
-    sp_r_m: {paddingRight: mod.$v.space_m},
-    sp_r_l: {paddingRight: mod.$v.space_l},
-    sp_r_xl: {paddingRight: mod.$v.space_xl},
+    sp_r_xs: () => ({paddingRight: mod.$v.space_xs}),
+    sp_r_s: () => ({paddingRight: mod.$v.space_s}),
+    sp_r_m: () => ({paddingRight: mod.$v.space_m}),
+    sp_r_l: () => ({paddingRight: mod.$v.space_l}),
+    sp_r_xl: () => ({paddingRight: mod.$v.space_xl}),
     /* Spacing Bottom */
-    sp_b_xs: {paddingBottom: mod.$v.space_xs},
-    sp_b_s: {paddingBottom: mod.$v.space_s},
-    sp_b_m: {paddingBottom: mod.$v.space_m},
-    sp_b_l: {paddingBottom: mod.$v.space_l},
-    sp_b_xl: {paddingBottom: mod.$v.space_xl},
+    sp_b_xs: () => ({paddingBottom: mod.$v.space_xs}),
+    sp_b_s: () => ({paddingBottom: mod.$v.space_s}),
+    sp_b_m: () => ({paddingBottom: mod.$v.space_m}),
+    sp_b_l: () => ({paddingBottom: mod.$v.space_l}),
+    sp_b_xl: () => ({paddingBottom: mod.$v.space_xl}),
     /* Spacing Left */
-    sp_l_xs: {paddingLeft: mod.$v.space_xs},
-    sp_l_s: {paddingLeft: mod.$v.space_s},
-    sp_l_m: {paddingLeft: mod.$v.space_m},
-    sp_l_l: {paddingLeft: mod.$v.space_l},
-    sp_l_xl: {paddingLeft: mod.$v.space_xl},
+    sp_l_xs: () => ({paddingLeft: mod.$v.space_xs}),
+    sp_l_s: () => ({paddingLeft: mod.$v.space_s}),
+    sp_l_m: () => ({paddingLeft: mod.$v.space_m}),
+    sp_l_l: () => ({paddingLeft: mod.$v.space_l}),
+    sp_l_xl: () => ({paddingLeft: mod.$v.space_xl}),
     /* Spacing Horizontal */
-    sp_h_xs: {paddingLeft: mod.$v.space_xs, paddingRight: mod.$v.space_xs},
-    sp_h_s: {paddingLeft: mod.$v.space_s, paddingRight: mod.$v.space_s},
-    sp_h_m: {paddingLeft: mod.$v.space_m, paddingRight: mod.$v.space_m},
-    sp_h_l: {paddingLeft: mod.$v.space_l, paddingRight: mod.$v.space_l},
-    sp_h_xl: {paddingLeft: mod.$v.space_xl, paddingRight: mod.$v.space_xl},
+    sp_h_xs: () => ({paddingLeft: mod.$v.space_xs, paddingRight: mod.$v.space_xs}),
+    sp_h_s: () => ({paddingLeft: mod.$v.space_s, paddingRight: mod.$v.space_s}),
+    sp_h_m: () => ({paddingLeft: mod.$v.space_m, paddingRight: mod.$v.space_m}),
+    sp_h_l: () => ({paddingLeft: mod.$v.space_l, paddingRight: mod.$v.space_l}),
+    sp_h_xl: () => ({paddingLeft: mod.$v.space_xl, paddingRight: mod.$v.space_xl}),
     /* Spacing Vertical */
-    sp_v_xs: {paddingTop: mod.$v.space_xs, paddingBottom: mod.$v.space_xs},
-    sp_v_s: {paddingTop: mod.$v.space_s, paddingBottom: mod.$v.space_s},
-    sp_v_m: {paddingTop: mod.$v.space_m, paddingBottom: mod.$v.space_m},
-    sp_v_l: {paddingTop: mod.$v.space_l, paddingBottom: mod.$v.space_l},
-    sp_v_xl: {paddingTop: mod.$v.space_xl, paddingBottom: mod.$v.space_xl},
+    sp_v_xs: () => ({paddingTop: mod.$v.space_xs, paddingBottom: mod.$v.space_xs}),
+    sp_v_s: () => ({paddingTop: mod.$v.space_s, paddingBottom: mod.$v.space_s}),
+    sp_v_m: () => ({paddingTop: mod.$v.space_m, paddingBottom: mod.$v.space_m}),
+    sp_v_l: () => ({paddingTop: mod.$v.space_l, paddingBottom: mod.$v.space_l}),
+    sp_v_xl: () => ({paddingTop: mod.$v.space_xl, paddingBottom: mod.$v.space_xl}),
     /* Margin */
-    sm_xs: {margin: mod.$v.space_xs},
-    sm_s: {margin: mod.$v.space_s},
-    sm_m: {margin: mod.$v.space_m},
-    sm_l: {margin: mod.$v.space_l},
-    sm_xl: {margin: mod.$v.space_xl},
+    sm_xs: () => ({margin: mod.$v.space_xs}),
+    sm_s: () => ({margin: mod.$v.space_s}),
+    sm_m: () => ({margin: mod.$v.space_m}),
+    sm_l: () => ({margin: mod.$v.space_l}),
+    sm_xl: () => ({margin: mod.$v.space_xl}),
     /* Margin Top */
-    sm_t_xs: {marginTop: mod.$v.space_xs},
-    sm_t_s: {marginTop: mod.$v.space_s},
-    sm_t_m: {marginTop: mod.$v.space_m},
-    sm_t_l: {marginTop: mod.$v.space_l},
-    sm_t_xl: {marginTop: mod.$v.space_xl},
-    sm_t_giga: {marginTop: 'calc(' + mod.$v.space_xl + ' * 2)'},
+    sm_t_xs: () => ({marginTop: mod.$v.space_xs}),
+    sm_t_s: () => ({marginTop: mod.$v.space_s}),
+    sm_t_m: () => ({marginTop: mod.$v.space_m}),
+    sm_t_l: () => ({marginTop: mod.$v.space_l}),
+    sm_t_xl: () => ({marginTop: mod.$v.space_xl}),
+    sm_t_giga: () => ({marginTop: 'calc(' + mod.$v.space_xl + ' * 2)'}),
     /* Margin Right */
-    sm_r_xs: {marginRight: mod.$v.space_xs},
-    sm_r_s: {marginRight: mod.$v.space_s},
-    sm_r_m: {marginRight: mod.$v.space_m},
-    sm_r_l: {marginRight: mod.$v.space_l},
-    sm_r_xl: {marginRight: mod.$v.space_xl},
+    sm_r_xs: () => ({marginRight: mod.$v.space_xs}),
+    sm_r_s: () => ({marginRight: mod.$v.space_s}),
+    sm_r_m: () => ({marginRight: mod.$v.space_m}),
+    sm_r_l: () => ({marginRight: mod.$v.space_l}),
+    sm_r_xl: () => ({marginRight: mod.$v.space_xl}),
     /* Margin Bottom */
-    sm_b_xs: {marginBottom: mod.$v.space_xs},
-    sm_b_s: {marginBottom: mod.$v.space_s},
-    sm_b_m: {marginBottom: mod.$v.space_m},
-    sm_b_l: {marginBottom: mod.$v.space_l},
-    sm_b_xl: {marginBottom: mod.$v.space_xl},
+    sm_b_xs: () => ({marginBottom: mod.$v.space_xs}),
+    sm_b_s: () => ({marginBottom: mod.$v.space_s}),
+    sm_b_m: () => ({marginBottom: mod.$v.space_m}),
+    sm_b_l: () => ({marginBottom: mod.$v.space_l}),
+    sm_b_xl: () => ({marginBottom: mod.$v.space_xl}),
     /* Margin Left */
-    sm_l_xs: {marginLeft: mod.$v.space_xs},
-    sm_l_s: {marginLeft: mod.$v.space_s},
-    sm_l_m: {marginLeft: mod.$v.space_m},
-    sm_l_l: {marginLeft: mod.$v.space_l},
-    sm_l_xl: {marginLeft: mod.$v.space_xl},
+    sm_l_xs: () => ({marginLeft: mod.$v.space_xs}),
+    sm_l_s: () => ({marginLeft: mod.$v.space_s}),
+    sm_l_m: () => ({marginLeft: mod.$v.space_m}),
+    sm_l_l: () => ({marginLeft: mod.$v.space_l}),
+    sm_l_xl: () => ({marginLeft: mod.$v.space_xl}),
     /* Margin Horizontal */
-    sm_h_xs: {marginLeft: mod.$v.space_xs, marginRight: mod.$v.space_xs},
-    sm_h_s: {marginLeft: mod.$v.space_s, marginRight: mod.$v.space_s},
-    sm_h_m: {marginLeft: mod.$v.space_m, marginRight: mod.$v.space_m},
-    sm_h_l: {marginLeft: mod.$v.space_l, marginRight: mod.$v.space_l},
-    sm_h_xl: {marginLeft: mod.$v.space_xl, marginRight: mod.$v.space_xl},
-    sm_h_auto: {marginLeft: 'auto', marginRight: 'auto'},
+    sm_h_xs: () => ({marginLeft: mod.$v.space_xs, marginRight: mod.$v.space_xs}),
+    sm_h_s: () => ({marginLeft: mod.$v.space_s, marginRight: mod.$v.space_s}),
+    sm_h_m: () => ({marginLeft: mod.$v.space_m, marginRight: mod.$v.space_m}),
+    sm_h_l: () => ({marginLeft: mod.$v.space_l, marginRight: mod.$v.space_l}),
+    sm_h_xl: () => ({marginLeft: mod.$v.space_xl, marginRight: mod.$v.space_xl}),
+    sm_h_auto: () => ({marginLeft: 'auto', marginRight: 'auto'}),
     /* Margin Vertical */
-    sm_v_xs: {marginTop: mod.$v.space_xs, marginBottom: mod.$v.space_xs},
-    sm_v_s: {marginTop: mod.$v.space_s, marginBottom: mod.$v.space_s},
-    sm_v_m: {marginTop: mod.$v.space_m, marginBottom: mod.$v.space_m},
-    sm_v_l: {marginTop: mod.$v.space_l, marginBottom: mod.$v.space_l},
-    sm_v_xl: {marginTop: mod.$v.space_xl, marginBottom: mod.$v.space_xl},
-    sm_v_auto: {marginTop: 'auto', marginBottom: 'auto'},
+    sm_v_xs: () => ({marginTop: mod.$v.space_xs, marginBottom: mod.$v.space_xs}),
+    sm_v_s: () => ({marginTop: mod.$v.space_s, marginBottom: mod.$v.space_s}),
+    sm_v_m: () => ({marginTop: mod.$v.space_m, marginBottom: mod.$v.space_m}),
+    sm_v_l: () => ({marginTop: mod.$v.space_l, marginBottom: mod.$v.space_l}),
+    sm_v_xl: () => ({marginTop: mod.$v.space_xl, marginBottom: mod.$v.space_xl}),
+    sm_v_auto: () => ({marginTop: 'auto', marginBottom: 'auto'}),
     /* Typographic */
-    text_giga_title: {
+    text_giga_title: () => ({
       fontFamily: mod.$v.font_header,
       fontWeight: 'bold',
       [mod.media.desktop]: {
@@ -312,24 +312,24 @@ const _css = createCss({
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_title} - .3rem)`,
       },
-    },
-    text_header: {
+    }),
+    text_header: () => ({
       fontFamily: mod.$v.font_header,
       fontWeight: 600,
       fontSize: mod.$v.font_s_header,
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_header} - .3rem)`,
       },
-    },
-    text_header_thin: {
+    }),
+    text_header_thin: () => ({
       fontFamily: mod.$v.font_header,
       fontWeight: 100,
       fontSize: mod.$v.font_s_header,
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_header} - .3rem)`,
       },
-    },
-    text_page_title: {
+    }),
+    text_page_title: () => ({
       fontFamily: mod.$v.font_header,
       fontWeight: 'bold',
       [mod.media.desktop]: {
@@ -341,86 +341,57 @@ const _css = createCss({
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_subtitle} - .2rem)`,
       },
-    },
-    text_form_label: {
+    }),
+    text_form_label: () => ({
       fontFamily: mod.$v.font_header,
       fontWeight: 600,
       fontSize: mod.$v.font_s_body,
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_body} - .2rem)`,
       },
-    },
-    text_body: {
+    }),
+    text_body: () => ({
       fontFamily: mod.$v.font_body,
       fontSize: mod.$v.font_s_body,
       fontWeight: 400,
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_body} - .2rem)`,
       },
-    },
-    text_body_thin: {
+    }),
+    text_body_thin: () => ({
       fontFamily: mod.$v.font_header,
       fontSize: mod.$v.font_s_body,
       fontWeight: 400,
       [mod.media.mobile]: {
         fontSize: `calc(${mod.$v.font_s_body} - .2rem)`,
       },
-    },
+    }),
     /* Theme */
-    badge_release: {
+    badge_release: () => ({
       background: mod.$t.badge_release_bg,
       color: mod.$t.badge_release_fg,
-    },
-    badge_blog: {
+    }),
+    badge_blog: () => ({
       background: mod.$t.badge_blog_bg,
       color: mod.$t.badge_blog_fg,
-    },
-    badge_normal: {
+    }),
+    badge_normal: () => ({
       background: mod.$t.badge_bg,
       color: mod.$t.badge_fg,
-    },
-    panel: {
+    }),
+    panel: () => ({
       background: mod.$t.panel_bg,
       color: mod.$t.panel_fg,
-    },
-    panel_alt: {
+    }),
+    panel_alt: () => ({
       background: mod.$t.panel_alt_bg,
       color: mod.$t.panel_alt_fg,
-    },
-    outline: {
+    }),
+    outline: () => ({
       [mod.hover]: {outline: mod.$t.outline},
-    },
+    }),
     /* Blocks */
-    page_wrapper: {
-      display: 'flex',
-      flexDirection: 'column',
-      justifyContent: 'center',
-      alignItems: 'center',
-      [mod.media.desktop]: {
-        paddingTop: mod.$v.space_xl,
-        paddingBottom: mod.$v.space_xl,
-        paddingLeft: mod.$v.space_m,
-        paddingRight: mod.$v.space_m,
-      },
-      [mod.media.tablet]: {
-        paddingTop: mod.$v.space_xl,
-        paddingBottom: mod.$v.space_xl,
-      },
-    },
-    page: {
-      display: 'flex',
-      flexDirection: 'column',
-      alignItems: 'flex-start',
-      [mod.media.desktop]: {
-        width: '90rem',
-        maxWidth: '90rem',
-      },
-      [mod.media.tablet]: {
-        width: '100%',
-        maxWidth: '100%',
-      },
-    },
-    button: {
+    button: () => ({
       appearance: 'none',
       border: 'none',
       fontFamily: mod.$v.font_body,
@@ -429,8 +400,8 @@ const _css = createCss({
       cursor: 'pointer',
       letterSpacing: '.1rem',
       span: {textDecoration: 'none'},
-    },
-    button_s: {
+    }),
+    button_s: () => ({
       paddingLeft: mod.$v.space_m,
       paddingRight: mod.$v.space_m,
       paddingTop: mod.$v.space_s,
@@ -444,8 +415,8 @@ const _css = createCss({
         paddingRight: mod.$v.space_s,
         fontSize: `calc(${mod.$v.font_s_body} - 0.2rem)`,
       },
-    },
-    button_l: {
+    }),
+    button_l: () => ({
       paddingLeft: mod.$v.space_l,
       paddingRight: mod.$v.space_l,
       paddingTop: mod.$v.space_m,
@@ -459,21 +430,19 @@ const _css = createCss({
         paddingRight: mod.$v.space_m,
         fontSize: `calc(${mod.$v.font_s_form} - 0.2rem)`,
       },
-    },
-    button_t_blue: {
+    }),
+    button_t_blue: () => ({
       background: mod.$t.button_bg,
       color: `${mod.$t.button_fg} !important`,
-      [mod.hover]: {
-        filter: 'brightness(1.2)',
-      },
-    },
-    button_t_grey: {
+      [mod.hover]: {filter: 'brightness(1.2)'},
+    }),
+    button_t_grey: () => ({
       background: mod.$t.badge_bg,
       color: mod.$t.badge_fg,
       [mod.hover]: {outline: mod.$t.outline},
-    },
+    }),
     /* Generic */
-    hide: {display: 'none'},
+    hide: () => ({display: 'none'}),
   }),
 });
 
